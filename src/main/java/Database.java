7ce7b4c788e8cafb1a77d8438c0a908810bfbd05
BartoszCzajczynski <b.czajczynski2@newcastle.ac.uk>
2022-03-04 11:33:17 +0000

    Added Test Question

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,5 +1,28 @@
 import csc1035.project2.HibernateUtil;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
 public class Database {
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        try{
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            Question q = new Question();
+            q.setTopic("Maths");
+            q.setType("SAQ");
+            q.setQuestionString("9+10?");
+            q.setAnswerString("21");
+            q.setMarks(2);
+            q.setIncorrectlyAnswered(false);
+            session.save(q);
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            if (session!=null) session.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
 }
---
+++ src/main/java/Question.java
@@ -0,0 +1,85 @@
+import javax.persistence.*;
+
+@Entity(name = "QUESTION")
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private int questionID;
+
+    @Column
+    private String questionString;
+
+    @Column
+    private String topic;
+
+    @Column
+    private String type;
+
+    @Column
+    private String answerString;
+
+    @Column
+    private int marks;
+
+    @Column
+    private boolean incorrectlyAnswered;
+
+    public Question(){}
+
+    public int getQuestionID() {
+        return questionID;
+    }
+
+    public void setQuestionID(int questionID) {
+        this.questionID = questionID;
+    }
+
+    public String getQuestionString() {
+        return questionString;
+    }
+
+    public void setQuestionString(String questionString) {
+        this.questionString = questionString;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAnswerString() {
+        return answerString;
+    }
+
+    public void setAnswerString(String answerString) {
+        this.answerString = answerString;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public boolean isIncorrectlyAnswered() {
+        return incorrectlyAnswered;
+    }
+
+    public void setIncorrectlyAnswered(boolean incorrectlyAnswered) {
+        this.incorrectlyAnswered = incorrectlyAnswered;
+    }
+}
