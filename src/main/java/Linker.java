a622e728f8e239c803ac6fe94cf98b9a6f973d69
BartoszCzajczynski <b.czajczynski2@newcastle.ac.uk>
2022-03-08 12:06:20 +0000

    Added Quiz to ManyToMany

--- src/main/java/Linker.java
+++ src/main/java/Linker.java
@@ -1,8 +1,14 @@
 import javax.persistence.*;
+import java.util.List;
 
 @Entity(name = "LINKER")
 public class Linker{
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private int linkId;
+
+
 
 }
 
---
+++ src/main/java/Quiz.java
@@ -0,0 +1,56 @@
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity(name = "QUIZ")
+public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private int quizID;
+
+    @Column
+    private String topicOfQuiz;
+
+    @Column
+    private int lengthOfQuiz;
+
+    @ManyToMany
+    @JoinTable(
+            name = "QUESTION",
+            joinColumns = {@JoinColumn(name = "quizId")},
+            inverseJoinColumns = {@JoinColumn(name = "questionId")})
+    private Set<Question> questions = new HashSet<>();
+
+    public int getQuizID() {
+        return quizID;
+    }
+
+    public void setQuizID(int quizID) {
+        this.quizID = quizID;
+    }
+
+    public String getTopicOfQuiz() {
+        return topicOfQuiz;
+    }
+
+    public void setTopicOfQuiz(String topicOfQuiz) {
+        this.topicOfQuiz = topicOfQuiz;
+    }
+
+    public int getLengthOfQuiz() {
+        return lengthOfQuiz;
+    }
+
+    public void setLengthOfQuiz(int lengthOfQuiz) {
+        this.lengthOfQuiz = lengthOfQuiz;
+    }
+
+    public Set<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(Set<Question> questions) {
+        this.questions = questions;
+    }
+}
