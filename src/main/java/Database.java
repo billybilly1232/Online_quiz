a8d930331f85e8dfaeec414070777c0b7e5794c9
t34Addict <c1016129@newcastle.ac.uk>
2022-03-09 12:04:49 +0000

    added blank methods for CRUD of questions. CRUD of quizzes and CD od logs to be filled in later

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -14,10 +14,6 @@ delete questions
     public void createQuestion(Question questionDetails) {
         // need to figure out how to add the created question to the db
         try {
-            /*
-            test for setting a question to the db with hardcoded data.
-            It did work
-             */
             // creates a new session with the database
             session = HibernateUtil.getSessionFactory().openSession();
             // begins the transaction
@@ -33,5 +29,41 @@ delete questions
             session.close();
             // closes the session
         }
+    }
+
+    public void readQuestion() {
+        // method for reading/viewing a question
+    }
+
+    public void updateQuestion() {
+        // method for updating/editing a question
+    }
+
+    public void deleteQuestion(){
+        // method for deleting a question
+    }
+
+    public void createQuiz() {
+        // method for creating a quiz
+    }
+
+    public void readQuiz() {
+        // method for reading/viewing a quiz
+    }
+
+    public void updateQuiz() {
+        // method for updating/editing a quiz
+    }
+
+    public void deleteQuiz(){
+        // method for deleting a quiz
+    }
+
+    public void createLog(){
+        // method for creating a log
+    }
+
+    public void deleteLog(){
+        // method for deleting a log
     }
 }
