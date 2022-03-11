e739339e105dd53253e0f7f44c079001167fded8
BartoszCzajczynski <b.czajczynski2@newcastle.ac.uk>
2022-03-11 10:11:49 +0000

    Added CRUD for question

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,9 +1,11 @@
 import csc1035.project2.HibernateUtil;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class Database {
-    Session session = HibernateUtil.getSessionFactory().openSession();
 /*
 create question
 read/list questions
@@ -13,34 +15,96 @@ delete questions
 
     public void createQuestion(Question questionDetails) {
         // need to figure out how to add the created question to the db
+        Session s = null;
         try {
             // creates a new session with the database
-            session = HibernateUtil.getSessionFactory().openSession();
+            s = HibernateUtil.getSessionFactory().openSession();
             // begins the transaction
-            session.beginTransaction();
-            session.save(questionDetails);
+            s.beginTransaction();
+            s.save(questionDetails);
             // saves the transaction
-            session.getTransaction().commit();
+            s.getTransaction().commit();
         } catch (HibernateException e) {
             // if something goes wrong, rollback to the previous transaction
-            if (session != null) session.getTransaction().rollback();
+            if (s != null) s.getTransaction().rollback();
             e.printStackTrace();
         } finally {
-            session.close();
-            // closes the session
+            if (s != null) {
+                s.close();
+                // closes the session
+            }
         }
     }
 
-    public void readQuestion() {
+    public Question readQuestion(int questionID) {
         // method for reading/viewing a question
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        s.beginTransaction();
+        Query questionQuery = s.createQuery("from QUESTION q where q.questionID = :question");
+        questionQuery.setParameter("question", questionID);
+        s.getTransaction().commit();
+        Question question = (Question) questionQuery.getResultList().get(0);
+        s.close();
+        return question;
     }
 
-    public void updateQuestion() {
+    public void updateQuestion(int questionID,String detailType, String updateString) {
         // method for updating/editing a question
+        Session s = null;
+        try {
+            s = HibernateUtil.getSessionFactory().openSession();
+            s.beginTransaction();
+            Question q = (s.get(Question.class, questionID));
+
+            switch (detailType){
+                case "Topic":{
+                    q.setTopicOfQuestion(updateString);
+                }
+                case "Type":{
+                    q.setTypeOfQuestion(updateString);
+                }
+                case "Question":{
+                    q.setQuestion(updateString);
+                }
+                case "Answer":{
+                    q.setAnswer(updateString);
+                }
+                case "Marks":{
+                    q.setMarks(Integer.parseInt(updateString));
+                }
+                case "IncorrectlyAnswered":{
+                    q.setIncorrectlyAnswered(Boolean.parseBoolean(updateString));
+                }
+            }
+            s.update(q);
+            s.getTransaction().commit();
+        } catch (HibernateException e) {
+            if (s != null) s.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+        }
     }
 
-    public void deleteQuestion(){
+    public void deleteQuestion(int questionID) {
         // method for deleting a question
+        Session s = null;
+        try {
+            s = HibernateUtil.getSessionFactory().openSession();
+            s.beginTransaction();
+            Question q = s.get(Question.class, questionID);
+            s.delete(q);
+            s.getTransaction().commit();
+        } catch (HibernateException e) {
+            if (s != null) s.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+        }
     }
 
     public void createQuiz() {
